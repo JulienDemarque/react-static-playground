1761c0b0596e7a735eb915102af52b94b960fe3c
julien demarque <julien_demarque@hotmail.fr>
2018-11-01 01:38:46 +0200

    query using variable given by prop

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ const App = () => (
         <div className="content">
           <Routes />
         </div>
-        <GetDataFromHasura />
+        <GetDataFromHasura keyword="ch" />
         <AddProfile />
       </div>
     </Router>
--- src/containers/Query.js
+++ src/containers/Query.js
@@ -2,40 +2,52 @@ import React from 'react'
 import { Query } from 'react-apollo'
 import gql from 'graphql-tag'
 
-const GetDataFromHasura = () => (
-  <div>
-    <h3>Query:</h3>
-    <p>
-      {`{
-    profile(where: { name: { _like: "%j%" } }) {
+const keyword = 'c'
+
+const GET_PROFILE = gql`
+  {
+    profile(where: { name: { _like: "%${keyword}%" } }) {
       id
       name
     }
-  }`}
-    </p>
-    <h3>Results:</h3>
-    <Query
-      query={gql`
-        {
-          profile(where: { name: { _like: "%j%" } }) {
-            id
-            name
-          }
-        }
-      `}
-    >
-      {({ loading, error, data }) => {
-        if (loading) return <p>Loading...</p>
-        if (error) return <p>Error :(</p>
+  }
+`
 
-        return data.profile.map(({ name }) => (
-          <div key={name}>
-            <p>{`${name}`}</p>
-          </div>
-        ))
-      }}
-    </Query>
-  </div>
-)
+const GetDataFromHasura = ({ keyword }) => {
+  const GET_PROFILE = gql`
+    {
+      profile(where: { name: { _like: "%${keyword}%" } }) {
+        id
+        name
+      }
+    }
+  `
+  return (
+    <div>
+      <h3>Query:</h3>
+      <p>
+        {`{
+      profile(where: { name: { _like: "%ju%" } }) {
+        id
+        name
+      }
+    }`}
+      </p>
+      <h3>Results:</h3>
+      <Query query={GET_PROFILE}>
+        {({ loading, error, data }) => {
+          if (loading) return <p>Loading...</p>
+          if (error) return <p>Error :(</p>
+
+          return data.profile.map(({ name }) => (
+            <div key={name}>
+              <p>{`${name}`}</p>
+            </div>
+          ))
+        }}
+      </Query>
+    </div>
+  )
+}
 
 export default GetDataFromHasura
